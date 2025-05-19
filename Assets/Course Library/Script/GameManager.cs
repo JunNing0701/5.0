128778579a7cd92d3723de7cd96d7333867c6cc0
Junning <yrzdzxggz@gmail.com>
2025-05-18 22:48:39 -0700

    chore: push to upstream

--- Assets/Course Library/Script/GameManager.cs
+++ Assets/Course Library/Script/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> target;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreTest;
+    private int score;
     IEnumerator SpawnTarget()
     {
         while (true) 
@@ -13,12 +16,16 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, target.Count);
             Instantiate(target[index]);
+            UpdateScore(5);
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
+        scoreTest.text = "Score: " + score;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
@@ -26,5 +33,10 @@ public class GameManager : MonoBehaviour
     {
         
     }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreTest.text = "Score: " + score;
+        
+    }
 }
-e
